5ccb22ee55a8594d6c7bdda1d41627c57fab6c1f
marundu <marundu@gmail.com>
2018-07-23 16:39:22 +0300

    This is logic, actual code not working

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
+# Consider this code logic. It is not working. 
+# I wrote it to help me understand what I'm doing. 
+
 from app import db
 
 class Book(db.Model):
+    
+    __tablename__='books'
   
     id=db.Column(db.Integer, primary_key=True)
     title=db.Column(db.String(100))
     author=db.Column(db.String(50))
     category=db.Column(db.String(20))
     done=db.Column(db.Boolean, default=False)
+    category_id=db.Column(db.Integer, db.ForeignKey('categories.id'))
     
     def __init__(self, title, author, category):
         self.title=title
@@ -16,3 +22,17 @@ class Book(db.Model):
     
     def __repr__(self):
         return '<Book: {0}>'.format(self.title)
+        
+class Category(db.Model):
+    
+    __tablename__='categories'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    category=db.Column(db.String(20))
+    books=db.relationship('Book', backref='category', lazy='dynamic')
+    
+    def __init__(self, category):
+        self.category=category
+    
+    def __repr__(self):
+        return '<Category: {0}>'.format(self.category)
