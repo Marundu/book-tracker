994fe72555abd2552f2501e5a9b4fe5b50a86fee
marundu <marundu@gmail.com>
2018-08-28 20:26:19 +0300

    Layout improvements

--- app/forms.py
+++ app/forms.py
@@ -1,27 +1,27 @@
-from flask_wtf import FlaskForm
+from flask_wtf import Form
 from wtforms import BooleanField, PasswordField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
-class RegisterForm(FlaskForm):
+class RegisterForm(Form):
     email=StringField('Email', validators=[DataRequired(), Email(), Length(min=8, max=40)])
     username=StringField('Username', validators=[DataRequired(), Length(min=8, max=40)])
     password=PasswordField('Password', validators=[DataRequired(), Length(min=8, max=40)])
     confirm=PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-class LoginForm(FlaskForm):
+class LoginForm(Form):
     # email=StringField('Email', validators=[DataRequired(), Email()])
     username=StringField('Username', validators=[DataRequired()])
     password=PasswordField('Password', validators=[DataRequired()])
     remember_me=BooleanField('Remember Me?')
 
-class EmailForm(FlaskForm):
+class EmailForm(Form):
     email=StringField('Email', validators=[DataRequired(), Email(), Length(min=8, max=40)])
 
-class PasswordForm(FlaskForm):
+class PasswordForm(Form):
     password=PasswordField('Password', validators=[DataRequired()])
     confirm=PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
-class EditProfileForm(FlaskForm):
+class EditProfileForm(Form):
     username=StringField('Username', validators=[DataRequired()])
     about_me=TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit=SubmitField('Submit')
