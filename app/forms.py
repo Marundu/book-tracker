74222173b3d457e5a74ddc470dd80f0eefba2502
chipomwitu <chipomwitu88@gmail.com>
2019-11-18 15:28:06 +0300

    improved language -> Submit to Save Changes

--- app/forms.py
+++ app/forms.py
@@ -59,7 +59,7 @@ class PasswordForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username=StringField('Username', validators=[DataRequired()])
     about_me=TextAreaField('About me', validators=[Length(min=0, max=140)])
-    submit=SubmitField('Submit')
+    submit=SubmitField('Save Changes')
 
 
 class BookForm(FlaskForm):
